c08ce6aa7b23a31ed926e39bec779f285aaa060c
sofialnvk <sofialinevik@hotmail.com>
2017-03-07 14:13:34 +0100

    Added folders for better structure

---
+++ Script/index.js
@@ -0,0 +1,8 @@
+var app = angular.module("TDDB", ["ngRoute"]);
+  app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {templateUrl : "main.html"})
+    .when("/page_search", {templateUrl : "../Source/search.html"})
+    .when("/page_add", {templateUrl : "../Source/add_files.html"})
+    .when("/page_test", {templateUrl : "../Source/test.html"});
+  });
--- index.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("TDDB", ["ngRoute"]);
-  app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {templateUrl : "main.html"})
-    .when("/page_search", {templateUrl : "search.html"})
-    .when("/page_add", {templateUrl : "add_files.html"})
-    .when("/page_test", {templateUrl : "test.html"});
-  });
