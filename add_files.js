47541818dda08e56fe96d9bcd78af0aac2ade14c
sofialnvk <sofialinevik@hotmail.com>
2017-03-07 11:15:55 +0100

    Edited code according to our style guide

--- add_files.js
+++ add_files.js
@@ -1,6 +1,18 @@
-    $(document).ready( function() {
-        $(':file').on('fileselect', function(event, numFiles, label) {
-            console.log(numFiles);
-            console.log(label);
-        });
-    });
+$(document).ready( function() {
+	$(':file').on('fileselect', function(event, numFiles, label) {
+		console.log(numFiles);
+		console.log(label);
+	});
+});
+
+$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+});
+
+$('.bootstrap-tagsinput').focusin(function() {
+	$(this).addClass('focus');
+});
+$('.bootstrap-tagsinput').focusout(function() {
+	$(this).removeClass('focus');
+});
