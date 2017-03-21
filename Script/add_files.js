1e5b750b8341a350f24a4572827d4a9b6ed98678
Jenny Tellne <jenny.tellne@gmail.com>
2017-03-21 14:23:43 +0100

    Changed file selection

--- Script/add_files.js
+++ Script/add_files.js
@@ -1,18 +1,42 @@
-$(document).ready( function() {
-	$(':file').on('fileselect', function(event, numFiles, label) {
-		console.log(numFiles);
-		console.log(label);
-	});
-});
+//Function to handle selected files from computer
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
 
-$("#menu-toggle").click(function(e) {
-	e.preventDefault();
-	$("#wrapper").toggleClass("toggled");
-});
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
 
-$('.bootstrap-tagsinput').focusin(function() {
-	$(this).addClass('focus');
-});
-$('.bootstrap-tagsinput').focusout(function() {
-	$(this).removeClass('focus');
-});
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      //To read the chosen file
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
