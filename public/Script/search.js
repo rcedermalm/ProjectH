74ad7d629d4cf574f34e81239da5871f631dd788
rcedermalm <rebecca@cedermalm.se>
2017-03-28 10:44:44 +0200

    table fix and document fix

--- public/Script/search.js
+++ public/Script/search.js
@@ -1,8 +1,19 @@
-var sectraAPI = "http://teatime.westeurope.cloudapp.azure.com/teatimewebapi/api/v0/Search/TestData?q=cr";
+var sectraAPI = "http://teatime.westeurope.cloudapp.azure.com/teatimewebapi/api/v0/Search/TestData?q=*";
 
-//var show_data_app = angular.module('show_data_app', []);
 app.controller('show_data_ctrl', function($scope, $http) {
     $http.get(sectraAPI).then(function(response) {
-        $scope.show_data = response.data.Result;
+        var the_data = response.data.Result;
+        console.log(the_data);
+        /*var the_data = new Array();
+        for(var i = 0; i < response.data.Result.length;i++){
+            //var patient = encodeURI(response.data.Result.Document.(0010,0010));
+            the_data.push({
+                "type": response.data.Result.Type,
+                "id": response.data.Result.Id,
+                //"patient_name": response.data.Result.Document + ".(0010,0010)"
+                //"patient_name": patient
+            })
+        }*/
+        $scope.show_data = the_data;
     });
 });
