7b863cda5f2e942ff39b06f67c371a912b638ad8
Ralphmedf <bollbergo@gmail.com>
2017-04-11 10:38:12 +0200

    Now works with adding files!
    Happy

--- public/Script/add_files.js
+++ public/Script/add_files.js
@@ -55,14 +55,49 @@ $('.bootstrap-tagsinput').focusout(function() {
   $(this).removeClass('focus');
 });
 
+
+
+// Post files in the database using POST in storeRequest
+// url: C:\\temp\\SharedStorage\\nyTest123
+
 // Create angular controller and pass in $scope and $http
 // This handles the input from the user, it is possible to add more paramenters
 // for the user input
 app.controller('AddController', function($scope, $http) {
+
+
+  $scope.SendData = function () {
+     // use $.param jQuery function to serialize data from JSON 
+      var data = { 'Directory': $scope.inputDirectory,
+        'Types': [$scope.inputTags],
+        'ErrorActionPreference': 0
+      };
+      console.log(data);
+
+      var config = {
+          headers : {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          }
+      }
+
+      $http.post('http://teatime.westeurope.cloudapp.azure.com/teatimewebapi/api/v0/StoreRequests', data, config)
+      .success(function (data, status, headers, config) {
+          $scope.PostDataResponse = data;
+          $scope.content = "Success.";
+      })
+      .error(function (data, status, headers, config) {
+        $scope.ResponseDetails = "Data: " + data +
+          "<hr />status: " + status +
+          "<hr />headers: " + headers +
+          "<hr />config: " + config;
+        $scope.content = "Failure.";
+      });
+    };
   
    // create a blank object to hold our form information
     // $scope will allow this to pass between controller and view
-      $scope.formData = {};
+    $scope.formData = {};
 
     // process the form
     $scope.processForm = function() {
