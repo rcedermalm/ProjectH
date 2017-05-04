6a28cc4ddb94a9a293a35f0bdcebd9489982e382
Jenny Tellne <jenny.tellne@gmail.com>
2017-05-04 13:43:32 +0200

    Added small changes, removed start page

--- public/Script/index.js
+++ public/Script/index.js
@@ -1,7 +1,7 @@
 var app = angular.module("TDDB", ["ngRoute"]);
   app.config(function($routeProvider) {
     $routeProvider
-    .when("/", {templateUrl : "Source/main.html"})
+    .when("/", {templateUrl : "Source/search.html"})
     .when("/page_search", {templateUrl : "Source/search.html"})
     .when("/page_add", {templateUrl : "Source/add_files.html"})
     .when("/page_test", {templateUrl : "Source/test.html"});
